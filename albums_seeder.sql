6aa419252a3f7ac35fe14a9e0ae700fa420c5d2b
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-09 08:33:26 -0500

    added albums with sales of 40 million or more to albums_seeder.sql file

---
+++ albums_seeder.sql
@@ -0,0 +1,22 @@
+USE codeup_test_db;
+VALUES ('Michael Jackson', 'Thriller', '1982', 47.3, 'Pop, Rock, R&B');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', '1976', 41.2, 'Country rock, Soft rock, Folk rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('AC/DC', 'Back in Black', '1980', 26.1, 'Hard rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', '1973', 24.2, 'Progressive rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Whitney Houston', 'The Bodyguard', '1992', 28.4, 'R&B, Soul, Pop, Soundtrack');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Meat Loaf', 'Bat Out of Hell', '1977', 21.5, 'Hard rock, Glam rock, Heavy Metal');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Hotel California', '1976', 31.5, 'Soft rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bee Gees', 'Saturday Night Fever', '1977', 21.6, 'Disco');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Fleetwood Mac', 'Rumours', '1977', 27.9, 'Soft rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Shania Twain', 'Come On Over', '1997', 29.6, 'Country, Pop');
+
