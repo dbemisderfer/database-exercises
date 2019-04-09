286538298aa98b9f682ab545c3d1355677d8a3d1
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-09 11:22:51 -0500

    added select statements after each update statement in update_exercises.sql file

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,9 +1,12 @@
 SELECT 'All albums in table' AS 'Caption 1';
 SELECT name FROM albums;
 UPDATE albums SET sales * 10;
+SELECT name FROM albums;
 SELECT 'All albums released before 1980' AS 'Caption 2';
 SELECT name FROM albums WHERE release_date < 1980;
 UPDATE albums SET release_date - 100 WHERE release_date < 1980;
+SELECT name FROM albums WHERE release_date < 1980;
 SELECT 'All albums by Michael Jackson' AS 'Caption 3';
 SELECT name FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums SET name = "Peter Jackson" WHERE name = "Michael Jackson";
+SELECT name FROM albums WHERE artist = 'Michael Jackson';
