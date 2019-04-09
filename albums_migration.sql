0f591cb01bccafecba656eccae67b33d86b7f439
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-09 11:05:16 -0500

    completion of select_exercises.sql file

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,9 @@
+DROP DATABASE IF EXISTS codeup_test_db;
+CREATE DATABASE codeup_test_db;
 USE codeup_test_db;
+
 DROP TABLE IF EXISTS albums;
+
 CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   artist VARCHAR(255) NOT NULL,
