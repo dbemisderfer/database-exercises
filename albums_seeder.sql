69e552f616118420f7bbf25b084fbc9f48a5a0de
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-09 11:57:01 -0500

    refactored albums_seeder.sql file to use single insert statement for all records

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,65 +1,34 @@
 USE codeup_test_db;
 INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop, Rock, R&B');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 41.2, 'Country rock, Soft rock, Folk rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Whitney Houston', 'The Bodyguard', 1992, 28.4, 'R&B, Soul, Pop, Soundtrack');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 21.5, 'Hard rock, Glam rock, Heavy metal');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Bee Gees', 'Saturday Night Fever', 1977, 21.6, 'Disco');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, Pop');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Olivia Newton-John', 'Grease: The Original Soundtrack from the Motion Picture', '1978', 14.4, 'Rock and roll');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard rock, Heavy metal, Folk rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Bad', 1987, 19.3, 'Pop, Funk, Rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Alanis Morissette', 'Jagged Little Pill', 1995, 24.4, 'Alternative rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Celine Dion', 'Falling Into You', 1996, 20.2, 'Pop, Soft rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.2, 'Rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Eric Carmen', 'Dirty Dancing', 1987, 17.9, 'Pop, Rock, R&B');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Dangerous', 1991, 17.3, 'Pop, Rock, R&B');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Adele', '21', 2011, 25.3, 'Pop, Soul');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Celine Dion', 'Let''s Talk About Love', 1997, 19.3, 'Pop, Soft rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('The Beatles', '1', 2000, 23.4, 'Rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('ABBA', 'Gold: Greatest Hits', 1992, 23.0, 'Pop, Disco');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Madonna', 'The Immaculate Collection', 1990, 19.4, 'Pop, Dance');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('The Beatles', 'Abbey Road', 1969, 14.4, 'Rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 19.6, 'Heartland rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Dire Straits', 'Brothers in Arms', 1985, 17.7, 'Roots rock, Blues rock, Soft rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('James Horner', 'Titanic', 1997, 18.1, 'Film score');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Metallica', 'Metallica', 1991, 25.2, 'Heavy metal');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge, Alternative rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Pink Floyd', 'The Wall', 1979, 18.7, 'Progressive rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Santana', 'Supernatural', 1999, 20.5, 'Latin rock');
-INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES ('Guns N'' Roses', 'Appetite for Destruction', 1987, 21.6, 'Hard rock');
+VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop, Rock, R&B'),
+('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 41.2, 'Country rock, Soft rock, Folk rock'),
+('AC/DC', 'Back in Black', 1980, 26.1, 'Hard rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 28.4, 'R&B, Soul, Pop, Soundtrack'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 21.5, 'Hard rock, Glam rock, Heavy metal'),
+('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock'),
+('Bee Gees', 'Saturday Night Fever', 1977, 21.6, 'Disco'),
+('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
+('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, Pop'),
+('Olivia Newton-John', 'Grease: The Original Soundtrack from the Motion Picture', '1978', 14.4, 'Rock and roll'),
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard rock, Heavy metal, Folk rock'),
+('Michael Jackson', 'Bad', 1987, 19.3, 'Pop, Funk, Rock'),
+('Alanis Morissette', 'Jagged Little Pill', 1995, 24.4, 'Alternative rock'),
+('Celine Dion', 'Falling Into You', 1996, 20.2, 'Pop, Soft rock'),
+('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.2, 'Rock'),
+('Eric Carmen', 'Dirty Dancing', 1987, 17.9, 'Pop, Rock, R&B'),
+('Michael Jackson', 'Dangerous', 1991, 17.3, 'Pop, Rock, R&B'),
+('Adele', '21', 2011, 25.3, 'Pop, Soul'),
+('Celine Dion', 'Let''s Talk About Love', 1997, 19.3, 'Pop, Soft rock'),
+('The Beatles', '1', 2000, 23.4, 'Rock'),
+('ABBA', 'Gold: Greatest Hits', 1992, 23.0, 'Pop, Disco'),
+('Madonna', 'The Immaculate Collection', 1990, 19.4, 'Pop, Dance'),
+('The Beatles', 'Abbey Road', 1969, 14.4, 'Rock'),
+('Bruce Springsteen', 'Born in the U.S.A.', 1984, 19.6, 'Heartland rock'),
+('Dire Straits', 'Brothers in Arms', 1985, 17.7, 'Roots rock, Blues rock, Soft rock'),
+('James Horner', 'Titanic', 1997, 18.1, 'Film score'),
+('Metallica', 'Metallica', 1991, 25.2, 'Heavy metal'),
+('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge, Alternative rock'),
+('Pink Floyd', 'The Wall', 1979, 18.7, 'Progressive rock'),
+('Santana', 'Supernatural', 1999, 20.5, 'Latin rock'),
+('Guns N'' Roses', 'Appetite for Destruction', 1987, 21.6, 'Hard rock');
