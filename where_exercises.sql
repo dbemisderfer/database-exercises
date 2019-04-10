552e195e01e2582f3535a83f5781cdccd755ce07
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-09 20:50:44 -0500

    completion of mysql where exercise from where lesson

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,25 +1,47 @@
 USE employees;
 SELECT 'First names' AS 'Problem 2';
 SELECT * FROM employees
-WHERE first_name
-IN ('Irena', 'Vidya', 'Maya');
+WHERE ((first_name = 'Irena')
+OR (first_name = 'Vidya')
+OR (first_name = 'Maya'))
+AND gender = 'M';
+-- WHERE first_name
+-- IN ('Irena', 'Vidya', 'Maya');
 
 SELECT 'Last name starts with E' AS 'Problem 3';
 SELECT * FROM employees
-WHERE last_name
-LIKE ('E%');
+WHERE (last_name like 'E%')
+OR (last_name like '%e');
+-- WHERE last_name
+-- LIKE ('E%');
 
-SELECT 'Hired in 90s' AS 'Problem 4';
+SELECT 'Last name starts with E' AS 'Problem 3';
 SELECT * FROM employees
-WHERE hire_date
-LIKE ('199%');
+WHERE (last_name like 'E%')
+AND (last_name like '%e');
 
-SELECT 'Born on Christmas day' AS 'Problem 5';
+SELECT 'Last name starts with E' AS 'Problem 3';
 SELECT * FROM employees
-WHERE birth_date
-LIKE ('%12-25');
+WHERE last_name LIKE 'E%e';
+
+SELECT 'Hired in 90s and born on Christmas day' AS 'Problems 4 & 5';
+SELECT * FROM employees
+WHERE (hire_date LIKE '199%')
+AND (birth_date LIKE '%12-25');
+
+-- SELECT 'Hired in 90s' AS 'Problem 4';
+-- SELECT * FROM employees
+-- WHERE hire_date
+-- LIKE '199%';
+
+-- SELECT 'Born on Christmas day' AS 'Problem 5';
+-- SELECT * FROM employees
+-- WHERE birth_date
+-- LIKE '%12-25';
 
 SELECT 'Last name with a q in it' AS 'Problem 6';
 SELECT * FROM employees
-WHERE last_name
-LIKE ('%q%');
+WHERE (last_name LIKE '%q%')
+AND (last_name NOT LIKE '%qu%');
+-- WHERE last_name
+-- LIKE ('%q%');
