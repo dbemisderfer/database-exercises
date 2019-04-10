0dae92131d29507ddb1ce93c3ccd65ac048b2a4f
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-10 15:01:11 -0500

    completion of functions exercise from mysql lesson

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -13,7 +13,7 @@ ORDER BY emp_no DESC;
 
 SELECT 'Last name starts with E' AS 'Problem 2';
 -- SELECT * FROM employees
-SELECT CONCAT(first_name, " ", last_name)
+SELECT *, CONCAT(first_name, " ", last_name)
 AS full_name FROM employees
 WHERE (last_name like 'E%')
 AND (last_name like '%e')
@@ -21,14 +21,14 @@ ORDER BY emp_no DESC;
 
 SELECT 'Last name starts with E' AS 'Problem 2';
 -- SELECT * FROM employees
-SELECT CONCAT(first_name, " ", last_name)
+SELECT *, CONCAT(first_name, ' ', last_name)
 AS full_name FROM employees
 WHERE last_name LIKE 'E%e'
 ORDER BY emp_no DESC;
 
 SELECT 'Hired in 90s and born on Christmas day' AS 'Problem 3';
 -- SELECT * FROM employees
-SELECT DATEDIFF(CURDATE(), hire_date)
+SELECT *, DATEDIFF(CURDATE(), hire_date)
 AS total_days_at_company FROM employees
 WHERE (hire_date LIKE '199%')
 AND (birth_date LIKE '%12-25')
