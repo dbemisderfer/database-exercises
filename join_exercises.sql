14a80f23ee036782b7c6b08c12e9df959b96e792
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-11 19:25:10 -0500

    completion of exercise problem 3 from joins lesson

--- join_exercises.sql
+++ join_exercises.sql
@@ -8,3 +8,14 @@ JOIN employees AS e
   ON dm.emp_no = e.emp_no
 WHERE dm.to_date LIKE '9999-01-01'
 ORDER BY `Department Name`;
+
+SELECT d.dept_name AS 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) AS 'Manager Name'
+FROM departments AS d
+       JOIN dept_manager AS dm
+            ON dm.dept_no = d.dept_no
+       JOIN employees AS e
+            ON dm.emp_no = e.emp_no
+WHERE dm.to_date LIKE '9999-01-01'
+AND e.gender LIKE 'F'
+ORDER BY `Department Name`;
