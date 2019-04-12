148b0002fbd2e57d2ec302633b05ec806e793bda
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-12 08:35:27 -0500

    completion of bonus exercise 1 from sub-queries exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -27,3 +27,15 @@ AND e.emp_no IN (
   )
 AND dm.to_date LIKE '9999-01-01';
 
+SELECT d.dept_name
+FROM employees as e
+JOIN dept_manager dm on e.emp_no = dm.emp_no
+JOIN departments d on dm.dept_no = d.dept_no
+WHERE gender = 'F'
+AND e.emp_no IN (
+  SELECT e.emp_no
+  FROM dept_manager
+  )
+AND dm.to_date LIKE '9999-01-01'
+ORDER BY d.dept_name;
+
