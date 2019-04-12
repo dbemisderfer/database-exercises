19b5917c07d9e02dfe41879316f7c5e7c8957f79
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-12 08:16:50 -0500

    completion of problem 3 from sub-queries exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,3 +6,16 @@ WHERE hire_date IN (
   WHERE emp_no = 101010
   );
 
+-- SELECT * FROM employees
+
+
+SELECT first_name, last_name
+FROM employees AS e
+JOIN dept_manager dm on e.emp_no = dm.emp_no
+WHERE gender = 'F'
+AND e.emp_no IN (
+  SELECT e.emp_no
+  FROM dept_manager
+  )
+AND dm.to_date LIKE '9999-01-01';
+
